80a565deedc4cbaded5308924ee3b22b41e6c9b5
Codeagles <hcn0521@outlook.com>
2017-08-25 11:33:48 +0800

    更新方法

--- ImageCutUtil.java
+++ ImageCutUtil.java
@@ -12,7 +12,7 @@ import javax.imageio.ImageReader;
 public class ImageCutUtil {
 	
 	
-	public static boolean splitImage(String path) throws IOException {  
+	public static void splitImage(String path) throws IOException {  
 	      
 	    String originalImg = path;  
 	  
@@ -20,17 +20,12 @@ public class ImageCutUtil {
 	    File file = new File(originalImg);  
 	    FileInputStream fis = new FileInputStream(file);  
 	    BufferedImage image = ImageIO.read(fis);
-	    int rows = 0;
-	    int pxHeight =2000;
-	    if(image.getHeight()>pxHeight){
-	    	rows = image.getHeight()/pxHeight;
-	    }else{
-	    	return false;
-	    }
-	    int cols = 1;  
+	    //分割成4*4(16)个小图  
+	    int rows = 4;  
+	    int cols = 4;  
 	    int chunks = rows * cols;  
-//	  
-//	    // 计算每个小图的宽度和高度  
+	  
+	    计算每个小图的宽度和高度  
 	    int chunkWidth = image.getWidth() / cols;  
 	    int chunkHeight = image.getHeight() / rows;  
 	    int count = 0;  
@@ -39,7 +34,6 @@ public class ImageCutUtil {
 	        for (int y = 0; y < cols; y++) {  
 	            //设置小图的大小和类型  
 	            imgs[count] = new BufferedImage(chunkWidth, chunkHeight, image.getType());  
-	  
 	            //写入图像内容  
 	            Graphics2D gr = imgs[count++].createGraphics();  
 	            gr.drawImage(image, 0, 0,   
@@ -57,121 +51,47 @@ public class ImageCutUtil {
 	    for (int i = 0; i < imgs.length; i++) {  
 	        ImageIO.write(imgs[i], suffix[1], new File("D:\\image\\split\\"+suffix[0] + i + "."+suffix[1]));  
 	    }  
-	      
-	    return true;
+	    System.out.println("完成分割");
 	}
-//	    int rows = 0;
-//	    if(image.getHeight() >650){
-//	    	rows = image.getHeight()/650;
-//	    }else{
-//	    	rows=2;
-//	    }
-//	   
-//	   
-//	    int chunks = rows ;
-//	    
-//	 
-//	    int chunkWidth = image.getWidth();  
-//	    int chunkHeight = image.getHeight() / rows;  
-//	    int count = 0;  
-//	    BufferedImage imgs[] = new BufferedImage[chunks];  
-//	    for (int x = 0; x < rows; x++) {  
-//	       
-//	            //设置小图的大小和类型  
-//	            imgs[count] = new BufferedImage(chunkWidth, chunkHeight, image.getType());  
-//	  
-//	            //写入图像内容  
-//	            Graphics2D gr = imgs[count++].createGraphics();  
-//	            gr.drawImage(image, 0, 0,
-//	            		chunkWidth, chunkHeight,
-//	            		670*x,chunkHeight * x
-//	                   , null);  
-//	            gr.dispose();  
-//	        }  
-//	     
-//	  
-//	    // 输出小图  
-//	    for (int i = 0; i < imgs.length; i++) {  
-//	        ImageIO.write(imgs[i], "jpg", new File("D:\\image\\split\\img" + i + ".jpg"));  
-//	    }  
-//	      
-//	    System.out.println("完成分割！");  
 	
-
-//	    // 分割成4*4(16)个小图  
-//	    int rows = 4;  
-//	    int cols = 4;  
-//	    int chunks = rows * cols;  
+	public static void mergeImage() throws IOException {  
+	    int rows = 4;  
+	    int cols = 4;  
+	    int chunks = rows * cols;  
 	  
-	    // 计算每个小图的宽度和高度  
-//	    int chunkWidth = image.getWidth() / cols;  
-//	    int chunkHeight = image.getHeight() / rows;  
-//	    int count = 0;  
-//	    BufferedImage imgs[] = new BufferedImage[chunks];  
-//	    for (int x = 0; x < rows; x++) {  
-//	        for (int y = 0; y < cols; y++) {  
-//	            //设置小图的大小和类型  
-//	            imgs[count] = new BufferedImage(chunkWidth, chunkHeight, image.getType());  
-//	  
-//	            //写入图像内容  
-//	            Graphics2D gr = imgs[count++].createGraphics();  
-//	            gr.drawImage(image, 0, 0,   
-//	                    chunkWidth, chunkHeight,   
-//	                    chunkWidth* y, chunkHeight * x,   
-//	                    chunkWidth * y + chunkWidth,  
-//	                    chunkHeight * x + chunkHeight, null);  
-//	            gr.dispose();  
-//	        }  
-//	    }  
-//	  
-//	    // 输出小图  
-//	    for (int i = 0; i < imgs.length; i++) {  
-//	        ImageIO.write(imgs[i], "jpg", new File("D:\\image\\split\\img" + i + ".jpg"));  
-//	    }  
-//	      
-//	    System.out.println("完成分割！");  
-//	} 
-	
-	
-	
-//	public static void mergeImage() throws IOException {  
-//	    int rows = 4;  
-//	    int cols = 4;  
-//	    int chunks = rows * cols;  
-//	  
-//	    int chunkWidth, chunkHeight;  
-//	    int type;  
-//	  
-//	    //读入小图  
-//	    File[] imgFiles = new File[chunks];  
-//	    for (int i = 0; i < chunks; i++) {  
-//	        imgFiles[i] = new File("D:\\image\\split\\img" + i + ".jpg");  
-//	    }  
-//	  
-//	    //创建BufferedImage  
-//	    BufferedImage[] buffImages = new BufferedImage[chunks];  
-//	    for (int i = 0; i < chunks; i++) {  
-//	        buffImages[i] = ImageIO.read(imgFiles[i]);  
-//	    }  
-//	    type = buffImages[0].getType();  
-//	    chunkWidth = buffImages[0].getWidth();  
-//	    chunkHeight = buffImages[0].getHeight();  
-//	  
-//	    //设置拼接后图的大小和类型  
-//	    BufferedImage finalImg = new BufferedImage(chunkWidth * cols, chunkHeight * rows, type);  
-//	  
-//	    //写入图像内容  
-//	    int num = 0;  
-//	    for (int i = 0; i < rows; i++) {  
-//	        for (int j = 0; j < cols; j++) {  
-//	            finalImg.createGraphics().drawImage(buffImages[num], chunkWidth * j, chunkHeight * i, null);  
-//	            num++;  
-//	        }  
-//	    }  
-//	  
-//	    //输出拼接后的图像  
-//	    ImageIO.write(finalImg, "jpeg", new File("D:\\image\\finalImg.jpg"));  
-//	  
-//	    System.out.println("完成拼接！");  
-//	}  
+	    int chunkWidth, chunkHeight;  
+	    int type;  
+	  
+	    //读入小图  
+	    File[] imgFiles = new File[chunks];  
+	    for (int i = 0; i < chunks; i++) {  
+	        imgFiles[i] = new File("D:\\image\\split\\img" + i + ".jpg");  
+	    }  
+	  
+	    //创建BufferedImage  
+	    BufferedImage[] buffImages = new BufferedImage[chunks];  
+	    for (int i = 0; i < chunks; i++) {  
+	        buffImages[i] = ImageIO.read(imgFiles[i]);  
+	    }  
+	    type = buffImages[0].getType();  
+	    chunkWidth = buffImages[0].getWidth();  
+	    chunkHeight = buffImages[0].getHeight();  
+	  
+	    //设置拼接后图的大小和类型  
+	    BufferedImage finalImg = new BufferedImage(chunkWidth * cols, chunkHeight * rows, type);  
+	  
+	    //写入图像内容  
+	    int num = 0;  
+	    for (int i = 0; i < rows; i++) {  
+	        for (int j = 0; j < cols; j++) {  
+	            finalImg.createGraphics().drawImage(buffImages[num], chunkWidth * j, chunkHeight * i, null);  
+	            num++;  
+	        }  
+	    }  
+	  
+	    //输出拼接后的图像  
+	    ImageIO.write(finalImg, "jpeg", new File("D:\\image\\finalImg.jpg"));  
+	  
+	    System.out.println("完成拼接！");  
+	}  
 }
